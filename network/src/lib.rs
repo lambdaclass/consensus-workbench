ee0578dd6ca439c18ef9cbb0fa10f6733d28a0d6
facundo <facundo.olano@entropy1729.com>
2022-09-29 08:23:56 -0300

    copy network module from asonnino/hotstuff

--- network/src/lib.rs
+++ network/src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// Copyright(C) Facebook, Inc. and its affiliates.
+mod error;
+mod receiver;
+mod reliable_sender;
+mod simple_sender;
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+#[path = "tests/common.rs"]
+pub mod common;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use crate::receiver::{MessageHandler, Receiver, Writer};
+pub use crate::reliable_sender::{CancelHandler, ReliableSender};
+pub use crate::simple_sender::SimpleSender;
