848f2ef76d70cbd008f1adc6b028746b596254e0
Facundo Olano <facundo.olano@entropy1729.com>
2022-09-29 13:08:07 -0300

    Port networking code (#2)
    * copy network module from asonnino/hotstuff
    
    * echo working
    
    * improve logging
    
    * add comment

--- 01-single-node/src/client.rs
+++ 01-single-node/src/client.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, client!");
+use std::net::SocketAddr;
+
+use bytes::Bytes;
+use network::ReliableSender;
+use log::info;
+
+
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    simple_logger::SimpleLogger::new().env().with_level(log::LevelFilter::Info).init().unwrap();
+
+    // using a reliable sender to get a response back
+    let mut sender = ReliableSender::new();
+    let address = "127.0.0.1:6100".parse::<SocketAddr>().unwrap();
+    let message = "Hello, world!";
+    let cancel_handler = sender.send(address, Bytes::from(message)).await;
+
+    match cancel_handler.await {
+        Ok(response) => {
+            info!("received response: {:?}", response);
+            Ok(())
+        },
+        Err(error) => Err(error.into())
+    }
 }
--- 01-single-node/src/node.rs
+++ 01-single-node/src/node.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, node!");
+use futures::sink::SinkExt as _;
+use network::{Receiver, MessageHandler, Writer};
+use async_trait::async_trait;
+use bytes::Bytes;
+use std::error::Error;
+use log::info;
+
+use std::net::SocketAddr;
+
+#[derive(Clone)]
+struct PingHandler;
+
+#[async_trait]
+impl MessageHandler for PingHandler {
+    async fn dispatch(&self, writer: &mut Writer, message: Bytes) -> Result<(), Box<dyn Error>> {
+        info!("Received request {:?}", message);
+        // echo the same message to the client
+        writer.send(message).await.map_err(|e| e.into())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    simple_logger::SimpleLogger::new().env().with_level(log::LevelFilter::Info).init().unwrap();
+
+    let address = "127.0.0.1:6100".parse::<SocketAddr>().unwrap();
+    Receiver::spawn(address, PingHandler).await.unwrap()
 }
