9e6b4cb2ff86d7760ca736ed4b74045692e10024
facundo <facundo.olano@entropy1729.com>
2022-10-03 09:13:24 -0300

    remove unused ping message

--- src/command.rs
+++ src/command.rs
@@ -1,12 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub enum PingMessage {
-    Ping,
-    Pong,
-    Other(String),
-}
-
 #[derive(Debug, Serialize, Deserialize)]
 pub enum KeyValueCommand {
     Set { key: String, value: String },
