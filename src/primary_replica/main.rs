28bec25d58d5e3174680a8bad1a8623449b6099b
facundo <facundo.olano@entropy1729.com>
2022-10-05 16:45:04 -0300

    rename modules

---
+++ src/primary_replica/main.rs
@@ -0,0 +1,182 @@
+use clap::Parser;
+use lib::network::Receiver;
+use log::info;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+use crate::node::Node;
+
+mod node;
+
+#[derive(Parser)]
+#[clap(author, version, about)]
+struct Cli {
+    /// Run as replica
+    #[arg(long, value_parser)]
+    replica: bool,
+    /// The network port of the node where to send txs.
+    #[clap(short, long, value_parser, value_name = "UINT", default_value_t = 6100)]
+    port: u16,
+    /// The network address of the node where to send txs.
+    #[clap(short, long, value_parser, value_name = "UINT", default_value_t = IpAddr::V4(Ipv4Addr::LOCALHOST))]
+    address: IpAddr,
+    /// Where to replicate to, if not running as a replica.
+    #[clap(short, long, value_parser, value_name = "ADDR")]
+    replicate_to: Option<SocketAddr>,
+}
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+
+    info!("Node socket: {}:{}", cli.address, cli.port);
+
+    simple_logger::SimpleLogger::new()
+        .env()
+        .with_level(log::LevelFilter::Info)
+        .init()
+        .unwrap();
+
+    let address = SocketAddr::new(cli.address, cli.port);
+
+    let node = match cli.replica {
+        false => {
+            info!("Primary: Running as primary on {}.", address);
+
+            // TODO we will eventually handle multiple peers, but for now we keep passing the single replica
+            cli.replicate_to
+                .is_some()
+                .then(|| info!("Primary: Replicating to {}.", cli.replicate_to.unwrap()));
+
+            Node::primary(vec![cli.replicate_to.unwrap()], ".db_primary")
+        }
+        true => {
+            info!(
+                "Replica: Running as replica on {}, waiting for commands from the primary node...",
+                address
+            );
+            Node::backup(".db_replica")
+        }
+    };
+    Receiver::spawn(address, node).await.unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use lib::command::Command;
+    use std::fs;
+    use tokio::time::{sleep, Duration};
+
+    // since logger is meant to be initialized once and tests run in parallel,
+    // run this before anything because otherwise it errors out
+    #[ctor::ctor]
+    fn init() {
+        simple_logger::SimpleLogger::new()
+            .env()
+            .with_level(log::LevelFilter::Info)
+            .init()
+            .unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_only_primary_server() {
+        fs::remove_dir_all(".db_test_primary1").unwrap_or_default();
+        let address: SocketAddr = "127.0.0.1:6379".parse().unwrap();
+        Receiver::spawn(
+            address,
+            node::Node::primary(Vec::new(), ".db_test_primary1"),
+        );
+        sleep(Duration::from_millis(10)).await;
+
+        let reply = Command::Get {
+            key: "k1".to_string(),
+        }
+        .send_to(address)
+        .await
+        .unwrap();
+        assert!(reply.is_none());
+
+        let reply = Command::Set {
+            key: "k1".to_string(),
+            value: "v1".to_string(),
+        }
+        .send_to(address)
+        .await
+        .unwrap();
+        assert!(reply.is_some());
+        assert_eq!("v1".to_string(), reply.unwrap());
+
+        let reply = Command::Get {
+            key: "k1".to_string(),
+        }
+        .send_to(address)
+        .await
+        .unwrap();
+        assert!(reply.is_some());
+        assert_eq!("v1".to_string(), reply.unwrap());
+    }
+
+    #[tokio::test]
+    async fn test_replicated_server() {
+        fs::remove_dir_all(".db_test_primary2").unwrap_or_default();
+        fs::remove_dir_all(".db_test_backup2").unwrap_or_default();
+
+        let address_primary: SocketAddr = "127.0.0.1:6380".parse().unwrap();
+        let address_replica: SocketAddr = "127.0.0.1:6381".parse().unwrap();
+        Receiver::spawn(address_replica, node::Node::backup(".db_test_backup2"));
+        Receiver::spawn(
+            address_primary,
+            node::Node::primary(vec![address_replica], ".db_test_primary2"),
+        );
+        sleep(Duration::from_millis(10)).await;
+
+        // get null value
+        let reply = Command::Get {
+            key: "k1".to_string(),
+        }
+        .send_to(address_primary)
+        .await
+        .unwrap();
+        assert!(reply.is_none());
+
+        // set a value on primary
+        let reply = Command::Set {
+            key: "k1".to_string(),
+            value: "v1".to_string(),
+        }
+        .send_to(address_primary)
+        .await
+        .unwrap();
+        assert!(reply.is_some());
+        assert_eq!("v1".to_string(), reply.unwrap());
+
+        // get value on primary
+        let reply = Command::Get {
+            key: "k1".to_string(),
+        }
+        .send_to(address_primary)
+        .await
+        .unwrap();
+        assert!(reply.is_some());
+        assert_eq!("v1".to_string(), reply.unwrap());
+
+        // get value on replica to make sure it was replicated
+        let reply = Command::Get {
+            key: "k1".to_string(),
+        }
+        .send_to(address_replica)
+        .await
+        .unwrap();
+        assert!(reply.is_some());
+        assert_eq!("v1".to_string(), reply.unwrap());
+
+        // should fail since replica should not respond to set commands
+        let reply = Command::Set {
+            key: "k3".to_string(),
+            value: "_".to_string(),
+        }
+        .send_to(address_replica)
+        .await;
+        assert!(reply.is_err());
+    }
+}
--- src/primary_replica/node.rs
+++ src/primary_replica/node.rs
@@ -1,182 +1,97 @@
-use clap::Parser;
-use lib::network::Receiver;
+/// This module contains a basic implementation of a primary node message handler
+/// Every Set command to a primary node will be relayed on the `replica_socket` node
+/// TODO update this doc
+use anyhow::{anyhow, Result};
+use async_trait::async_trait;
+use bytes::Bytes;
+use futures::sink::SinkExt as _;
+use lib::{
+    network::{MessageHandler, ReliableSender, Writer},
+    store::Store,
+};
 use log::info;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::net::SocketAddr;
 
-use crate::primary::Node;
+use lib::command::Command;
 
-mod primary;
-
-#[derive(Parser)]
-#[clap(author, version, about)]
-struct Cli {
-    /// Run as replica
-    #[arg(long, value_parser)]
-    replica: bool,
-    /// The network port of the node where to send txs.
-    #[clap(short, long, value_parser, value_name = "UINT", default_value_t = 6100)]
-    port: u16,
-    /// The network address of the node where to send txs.
-    #[clap(short, long, value_parser, value_name = "UINT", default_value_t = IpAddr::V4(Ipv4Addr::LOCALHOST))]
-    address: IpAddr,
-    /// Where to replicate to, if not running as a replica.
-    #[clap(short, long, value_parser, value_name = "ADDR")]
-    replicate_to: Option<SocketAddr>,
+#[derive(Clone)]
+/// A message handler that just forwards key/value store requests from clients to an internal rocksdb store.
+pub struct Node {
+    pub state: State,
+    pub store: Store,
+    pub peers: Vec<SocketAddr>,
+    pub sender: ReliableSender,
 }
 
-#[tokio::main]
-async fn main() {
-    let cli = Cli::parse();
-
-    info!("Node socket: {}:{}", cli.address, cli.port);
-
-    simple_logger::SimpleLogger::new()
-        .env()
-        .with_level(log::LevelFilter::Info)
-        .init()
-        .unwrap();
-
-    let address = SocketAddr::new(cli.address, cli.port);
-
-    let node = match cli.replica {
-        false => {
-            info!("Primary: Running as primary on {}.", address);
-
-            // TODO we will eventually handle multiple peers, but for now we keep passing the single replica
-            cli.replicate_to
-                .is_some()
-                .then(|| info!("Primary: Replicating to {}.", cli.replicate_to.unwrap()));
-
-            Node::primary(vec![cli.replicate_to.unwrap()], ".db_primary")
-        }
-        true => {
-            info!(
-                "Replica: Running as replica on {}, waiting for commands from the primary node...",
-                address
-            );
-            Node::backup(".db_replica")
-        }
-    };
-    Receiver::spawn(address, node).await.unwrap();
+/// The state of a node viewed as a state-machine.
+#[derive(Clone, Copy)]
+pub enum State {
+    Primary,
+    Backup,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use lib::command::Command;
-    use std::fs;
-    use tokio::time::{sleep, Duration};
+use State::*;
 
-    // since logger is meant to be initialized once and tests run in parallel,
-    // run this before anything because otherwise it errors out
-    #[ctor::ctor]
-    fn init() {
-        simple_logger::SimpleLogger::new()
-            .env()
-            .with_level(log::LevelFilter::Info)
-            .init()
-            .unwrap();
+impl Node {
+    pub fn primary(peers: Vec<SocketAddr>, db_path: &str) -> Self {
+        Self {
+            state: Primary,
+            store: Store::new(db_path).unwrap(),
+            peers,
+            sender: ReliableSender::new(),
+        }
     }
 
-    #[tokio::test]
-    async fn test_only_primary_server() {
-        fs::remove_dir_all(".db_test_primary1").unwrap_or_default();
-        let address: SocketAddr = "127.0.0.1:6379".parse().unwrap();
-        Receiver::spawn(
-            address,
-            primary::Node::primary(Vec::new(), ".db_test_primary1"),
-        );
-        sleep(Duration::from_millis(10)).await;
-
-        let reply = Command::Get {
-            key: "k1".to_string(),
+    pub fn backup(db_path: &str) -> Self {
+        Self {
+            state: Backup,
+            store: Store::new(db_path).unwrap(),
+            peers: vec![],
+            sender: ReliableSender::new(),
         }
-        .send_to(address)
-        .await
-        .unwrap();
-        assert!(reply.is_none());
-
-        let reply = Command::Set {
-            key: "k1".to_string(),
-            value: "v1".to_string(),
-        }
-        .send_to(address)
-        .await
-        .unwrap();
-        assert!(reply.is_some());
-        assert_eq!("v1".to_string(), reply.unwrap());
-
-        let reply = Command::Get {
-            key: "k1".to_string(),
-        }
-        .send_to(address)
-        .await
-        .unwrap();
-        assert!(reply.is_some());
-        assert_eq!("v1".to_string(), reply.unwrap());
     }
+}
 
-    #[tokio::test]
-    async fn test_replicated_server() {
-        fs::remove_dir_all(".db_test_primary2").unwrap_or_default();
-        fs::remove_dir_all(".db_test_backup2").unwrap_or_default();
+#[async_trait]
+impl MessageHandler for Node {
+    async fn dispatch(&mut self, writer: &mut Writer, bytes: Bytes) -> Result<()> {
+        let request = bincode::deserialize(&bytes)?;
+        info!("Received request {:?}", request);
 
-        let address_primary: SocketAddr = "127.0.0.1:6380".parse().unwrap();
-        let address_replica: SocketAddr = "127.0.0.1:6381".parse().unwrap();
-        Receiver::spawn(address_replica, primary::Node::backup(".db_test_backup2"));
-        Receiver::spawn(
-            address_primary,
-            primary::Node::primary(vec![address_replica], ".db_test_primary2"),
-        );
-        sleep(Duration::from_millis(10)).await;
+        let result = match (self.state, request) {
+            (Primary, Command::Set { key, value }) => {
+                // TODO review: since we're always using the same serialization format,
+                // would it make sense to always handle serialization inside the networking calls?
+                let sync_message: Bytes = bincode::serialize(&Command::SyncSet {
+                    key: key.clone(),
+                    value: value.clone(),
+                })
+                .unwrap()
+                .into();
 
-        // get null value
-        let reply = Command::Get {
-            key: "k1".to_string(),
-        }
-        .send_to(address_primary)
-        .await
-        .unwrap();
-        assert!(reply.is_none());
+                // forward the command to all replicas and wait for them to respond
+                let handlers = self
+                    .sender
+                    .broadcast(self.peers.to_vec(), sync_message)
+                    .await;
+                futures::future::join_all(handlers).await;
 
-        // set a value on primary
-        let reply = Command::Set {
-            key: "k1".to_string(),
-            value: "v1".to_string(),
-        }
-        .send_to(address_primary)
-        .await
-        .unwrap();
-        assert!(reply.is_some());
-        assert_eq!("v1".to_string(), reply.unwrap());
+                self.store.write(key.into(), value.into()).await
+            }
+            (Backup, Command::SyncSet { key, value }) => {
+                self.store
+                    .write(key.clone().into(), value.clone().into())
+                    .await
+            }
+            (_, Command::Get { key }) => self.store.read(key.clone().into()).await,
+            _ => Err(anyhow!("Unhandled command")),
+        };
 
-        // get value on primary
-        let reply = Command::Get {
-            key: "k1".to_string(),
-        }
-        .send_to(address_primary)
-        .await
-        .unwrap();
-        assert!(reply.is_some());
-        assert_eq!("v1".to_string(), reply.unwrap());
+        // convert the error into something serializable
+        let result = result.map_err(|e| e.to_string());
 
-        // get value on replica to make sure it was replicated
-        let reply = Command::Get {
-            key: "k1".to_string(),
-        }
-        .send_to(address_replica)
-        .await
-        .unwrap();
-        assert!(reply.is_some());
-        assert_eq!("v1".to_string(), reply.unwrap());
-
-        // should fail since replica should not respond to set commands
-        let reply = Command::Set {
-            key: "k3".to_string(),
-            value: "_".to_string(),
-        }
-        .send_to(address_replica)
-        .await;
-        assert!(reply.is_err());
+        info!("Sending response {:?}", result);
+        let reply = bincode::serialize(&result)?;
+        Ok(writer.send(reply.into()).await?)
     }
 }
