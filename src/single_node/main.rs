ae5b9480c99714a5658e44d20ccd416862c577dc
Francisco Strambini <fstrambini@Franciscos-MacBook-Air.local>
2022-10-24 18:11:28 -0300

    first aproach on the single node

--- src/single_node/main.rs
+++ src/single_node/main.rs
@@ -4,14 +4,17 @@ use anyhow::Result;
 use async_trait::async_trait;
 use bytes::Bytes;
 use clap::Parser;
-use futures::sink::SinkExt as _;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
+use tokio::sync::oneshot;
 use lib::{
     command::ClientCommand,
     network::{MessageHandler, Receiver, Writer},
     store::Store,
 };
 use log::info;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::{net::{IpAddr, Ipv4Addr, SocketAddr}, thread::JoinHandle};
+
+pub const CHANNEL_CAPACITY: usize = 1_000;
 
 #[derive(Parser)]
 #[clap(author, version, about)]
@@ -63,12 +66,79 @@ async fn main() {
     simple_logger::SimpleLogger::new().env().init().unwrap();
 
     let address = SocketAddr::new(cli.address, cli.port);
-    let store = Store::new(".db_single_node").unwrap();
     tokio::spawn(async move {
         let receiver = Receiver::new(address, Node { store });
         receiver.run().await;
     });
 }
+
+async fn spawn_node_tasks(address: SocketAddr, seed: Option<SocketAddr>) -> (JoinHandle<()>, JoinHandle<()>) {
+    let (network_sender, network_receiver) = channel(CHANNEL_CAPACITY);
+
+    let store = Store::new(".db_single_node").unwrap();
+
+    let newtor_handle = tokio::spawn( async move {
+        let mut node = Node::new();
+        node.run(network_receiver);
+    });
+
+    let node_handle = tokio::spawn(async move {
+        let receiver = NetworkReceiver::new(address, NodeReceiverHandler { network_sender });
+        receiver.run().await;
+    });
+
+    (newtor_handle, newtor_handle)
+}
+
+
+impl Node {
+    pub fn new() -> Self {
+        Self {
+            store: Store::new(".db_single_node").unwrap(),
+        }
+    }
+
+    pub async fn run(&mut self, mut network_receiver: Receiver<(ClientCommand, oneshot::Sender<Result<Option<Vec<u8>>>>)>) -> () {
+        while let Some((message, reply_sender)) = network_receiver.recv().await {
+            let result = match message {
+                ClientCommand::Set { key, value } => {
+                        self.store
+                        .write(key.clone().into(), value.clone().into())
+                        .await
+                }
+                ClientCommand::Get { key } => self.store.read(key.clone().into()).await,
+            };
+
+            reply_sender.send(result);
+        }
+    }
+
+}
+
+
+#[derive(Clone)]
+struct NodeReceiverHandler {
+    /// Used to forward incoming TCP messages to the node
+    network_sender: Sender<(ClientCommand, oneshot::Sender<Result<Option<String>>>)>,
+}
+
+
+#[async_trait]
+impl MessageHandler for NodeReceiverHandler {
+    /// When a TCP message is received, interpret it as a node::Message and forward it to the node task.
+    /// Send the node's response back through the TCP connection.
+    async fn dispatch(&mut self, writer: &mut Writer, bytes: Bytes) -> Result<()> {
+        let request: ClientCommand = bincode::deserialize(&bytes)?;
+        info!("Received request {:?}", request);
+
+        let (reply_sender, reply_receiver) = oneshot::channel();
+        self.network_sender.send((request, reply_sender)).await?;
+        let reply = reply_receiver.await?.map_err(|e| e.to_string());
+        let reply = bincode::serialize(&reply)?;
+        Ok(writer.send(reply.into()).await?)
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
